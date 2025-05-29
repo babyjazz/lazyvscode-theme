6812a7c410db19afb39c77e7e5aba3637cbd7818
babyjazz <bbos1@windowslive.com>
2025-05-30 00:18:10 +0700

    fix trail begin invalid line

--- follow-cursor.js
+++ follow-cursor.js
@@ -21,7 +21,9 @@ const log = (msg) => {
   console.log("#################back followCursor #################");
 };
 
-let fromLineNumber = 0;
+const editor = vscode.window.activeTextEditor;
+const cursorPosition = editor.selection.active;
+let fromLineNumber = cursorPosition.line;
 let decorationTimeout = null;
 const minRange = 50;
 const tailLength = 8;
@@ -34,9 +36,8 @@ const followCursor = () => {
   const lineNumber = cursorPosition.line; // Add 1 since VSCode lines are 0-based
   const characterNumber = cursorPosition.character + 1; // Add 1 to make it 1-based like line numbers
 
-  log(`from ${fromLineNumber} to ${lineNumber}`);
   // ONLY TOP TO BOTTOM
-  if (Math.abs(fromLineNumber - lineNumber) >= minRange) {
+  if (lineNumber - fromLineNumber >= minRange) {
     let i = 0;
     const interval = setInterval(() => {
       const trail = fromLineNumber + i;
@@ -49,13 +50,36 @@ const followCursor = () => {
 
       editor.setDecorations(circleDecorationType, [{ range }]);
 
-      if (fromLineNumber + i >= lineNumber) {
+      log(`from ${trail} to ${lineNumber}`);
+      if (trail >= lineNumber) {
         clearInterval(interval);
+        fromLineNumber = lineNumber;
       } else {
         i++;
       }
     }, 10);
   }
+  // ONLY BOTTOM TO TOP
+  // if (fromLineNumber - lineNumber >= minRange) {
+  //   let i = 0;
+  //   const interval = setInterval(() => {
+  //     const trail = fromLineNumber + i;
+  //     const head = fromLineNumber + tailLength + i;
+  //   }, 10);
+
+  //   let range = new vscode.Range(trail, 0, head, 0);
+  //   if (head >= lineNumber) {
+  //     range = new vscode.Range(trail, 0, lineNumber, 0);
+  //   }
+
+  //   editor.setDecorations(circleDecorationType, [{ range }]);
+
+  //   if (fromLineNumber + i >= lineNumber) {
+  //     clearInterval(interval);
+  //   } else {
+  //     i++;
+  //   }
+  // }
 
   // Clear any existing timeout
   if (decorationTimeout) {
